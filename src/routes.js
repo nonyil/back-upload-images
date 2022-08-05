f216ce82b0d5722fab9b0e7ffd71a7afab6c1665
nonyil <igbrendow@gmail.com>
2022-08-05 15:47:40 -0300

    feat: :fire: add multer config
    multer upload image config

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
+const multer = require("multer");
 const routes = require("express").Router();
+const multerConfig = require("./config/multer");
 
-routes.get('/', (req, res) => {
-  res.json({ hello: 'World' });
+
+routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
+  console.log(req.file);
+  return res.json({ hello: 'World' });
 })
 
 
