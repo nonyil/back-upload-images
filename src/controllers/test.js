e9343ddec7d6c6e6968cbda34f30e84ba639d536
nonyil <igbrendow@gmail.com>
2022-08-05 15:21:44 -0300

    feat: :memo: New Application, making new configs
    index/routes

--- src/controllers/test.js
+++
@@ -1,8 +0,0 @@
-const create = require('../models/test');
-
-async function testCreate(request, response) {
-  const createdTest = await create()
-  return response.status(201).json(createdTest);
-}
-
-module.exports = testCreate;
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const routes = require("express").Router();
+
+routes.get('/', (req, res) => {
+  res.json({ hello: 'World' });
+})
+
+
+module.exports = routes;
